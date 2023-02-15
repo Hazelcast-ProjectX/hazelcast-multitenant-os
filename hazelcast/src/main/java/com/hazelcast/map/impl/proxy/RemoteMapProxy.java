c4a9a87b58a34619733dabe02345779d975b8839
Vassilis Bekiaris <vassilis@hazelcast.com>
2023-02-15 19:57:01 +0200

    Add RemoteMapProxy.getAsync

--- hazelcast/src/main/java/com/hazelcast/map/impl/proxy/RemoteMapProxy.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/proxy/RemoteMapProxy.java
@@ -4,6 +4,7 @@ import com.hazelcast.config.MapConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.impl.MapService;
 import com.hazelcast.query.Predicate;
+import com.hazelcast.spi.impl.InternalCompletableFuture;
 import com.hazelcast.spi.impl.NodeEngine;
 
 import javax.annotation.Nonnull;
@@ -20,6 +21,11 @@ public class RemoteMapProxy<K, V> extends MapProxyImpl<K, V> {
             throw new IllegalArgumentException("RemoteMapProxy cannot be instantiated without remote connection"
                     + " to storage cluster.");
         }
+    }
+
+    @Override
+    public InternalCompletableFuture<V> getAsync(@Nonnull K key) {
+        return (InternalCompletableFuture<V>) remoteClusterClient.getMap(prefixedMapName()).getAsync(key).toCompletableFuture();
     }
 
     @Nonnull
