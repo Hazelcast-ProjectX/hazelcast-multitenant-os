0cd57a8f7f18f6843761b9e5abd0771c2ba67abb
Frantisek Hartman <frant.hartm@gmail.com>
2023-02-15 18:07:10 +0100

    Add size() and keySet() to remote implementation

--- hazelcast/src/main/java/com/hazelcast/map/impl/proxy/RemoteMapProxy.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/proxy/RemoteMapProxy.java
@@ -6,9 +6,11 @@ import com.hazelcast.map.impl.MapService;
 import com.hazelcast.query.Predicate;
 import com.hazelcast.spi.impl.InternalCompletableFuture;
 import com.hazelcast.spi.impl.NodeEngine;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 import java.util.Collection;
+import java.util.Set;
 
 public class RemoteMapProxy<K, V> extends MapProxyImpl<K, V> {
 
@@ -37,6 +39,16 @@ public class RemoteMapProxy<K, V> extends MapProxyImpl<K, V> {
     @Override
     public Collection<V> values(@Nonnull Predicate predicate) {
         return (Collection<V>) remoteClusterClient.getMap(prefixedMapName()).values(predicate);
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return (Set<K>) remoteClusterClient.getMap(prefixedMapName()).keySet();
+    }
+
+    @Override
+    public int size() {
+        return remoteClusterClient.getMap(prefixedMapName()).size();
     }
 
     private String prefixedMapName() {
